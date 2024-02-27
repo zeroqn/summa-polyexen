33be77f6496172dd184964344810dbd1ae71def4
zeroqn <zeroqn@pm.me>
2024-02-27 08:24:28 +0000

    feat(chip): do bound analysis on mst chip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt,
     fs::File,
     io::{self, Write},
     path::Path,
@@ -6,7 +7,15 @@ use std::{
 
 use env_logger::Env;
 use halo2_proofs::{halo2curves::bn256::Fr, plonk::Circuit};
-use polyexen::plaf::{frontends::halo2::get_plaf, Plaf, PlafDisplayBaseTOML, PlafDisplayFixedCSV};
+use num_bigint::BigUint;
+use polyexen::{
+    analyze::{bound_base, find_bounds_poly, Analysis},
+    expr::ExprDisplay,
+    plaf::{
+        frontends::halo2::get_plaf, Cell, CellDisplay, Plaf, PlafDisplayBaseTOML,
+        PlafDisplayFixedCSV,
+    },
+};
 
 mod circuit;
 
@@ -20,8 +29,49 @@ fn main() {
 }
 
 fn do_circuit_analysis(k: u32, c: &impl Circuit<Fr>) {
-    let plaf = get_plaf(k, c).unwrap();
-    write_files("mst", &plaf).unwrap();
+    let mut plaf = get_plaf(k, c).unwrap();
+    plaf.simplify();
+    write_files("mst_plaf", &plaf).unwrap();
+
+    let p = BigUint::parse_bytes(b"100000000000000000000000000000000", 16).unwrap()
+        - BigUint::from(159u64);
+    let mut analysis = Analysis::new();
+
+    let cell_fmt =
+        |f: &mut fmt::Formatter<'_>, c: &Cell| write!(f, "{}", CellDisplay { c, plaf: &plaf });
+    for offset in 0..plaf.info.num_rows {
+        for poly in &plaf.polys {
+            let mut exp = plaf.resolve(&poly.exp, offset);
+            exp.simplify(&p);
+            if exp.is_zero() {
+                continue;
+            }
+            log::debug!(
+                "\"{}\" {}",
+                poly.name,
+                ExprDisplay {
+                    e: &exp,
+                    var_fmt: cell_fmt
+                }
+            );
+            // Fill bounds in analysis.vars_attrs
+            find_bounds_poly(&exp, &p, &mut analysis);
+        }
+    }
+    let bound_base = bound_base(&p);
+    for (cell, attrs) in &analysis.vars_attrs {
+        if attrs.bound == bound_base {
+            continue;
+        }
+        log::debug!(
+            "{} bound {:?}",
+            CellDisplay {
+                c: cell,
+                plaf: &plaf
+            },
+            attrs.bound
+        );
+    }
 }
 
 fn write_files(name: &str, plaf: &Plaf) -> Result<(), io::Error> {
